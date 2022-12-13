1c6fba13a9a37e02a3b06809c5c84bd81ce9b591
DaniBootCamp <titodani@gmail.com>
2022-12-13 20:55:46 +0100

    third upload, WIP

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // URL local de nuestra base de datos en mongoose y su nombre upgrade_mongoose
-const DB_URL = "mongodb://localhost:27017/proyecto_node";
+const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/proyecto_node";
 
 // Función que conecta nuestro servidor a la base de datos de MongoDB mediante mongoose
 const connect = () =>
