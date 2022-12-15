22227253012af3906401c4afe26ed8fb2e023264
DaniBootCamp <titodani@gmail.com>
2022-12-15 15:06:41 +0100

    11 upload, WIP

--- routes/cars.routes.js
+++ routes/cars.routes.js
@@ -1,96 +1,83 @@
 // Archivo character.routes.js dentro de la carpeta routes
-const express = require("express");
-const Car = require("../models/Car");
-const filesMiddleware = require("../middlewares/files.middleware");
+const express = require('express');
+const mongoose = require('mongoose');
+const Car = require('../models/Car');
+const filesMiddleware = require('../middlewares/files.middleware')
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const cars = await Car.find();
-    return res.status(200).json(cars);
-  } catch (err) {
-    return res.status(500).json(err);
-  }
+router.get('/', async (req, res, next) => {
+	try {
+		const cars = await Car.find();
+		return res.status(200).json(cars)
+	} catch (err) {
+		return res.status(500).json(err);
+	}
 });
 
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-    const car = await Car.findById(id);
-    if (car) {
-      return res.status(200).json(car);
-    } else {
-      return res.status(404).json("Car not found with this id");
-    }
-  } catch (err) {
-    return res.status(500).json(err);
-  }
-});
-
-router.get("/modelo/:modelo", async (req, res) => {
-  const { modelo } = req.params;
-
-  try {
-    const carModelo = await Car.find({ modelo: modelo });
-    return res.status(200).json(carModelo);
-  } catch (err) {
-    return res.status(500).json(err);
-  }
-});
-
-router.put('/edit/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params 
-        const carModify = new Car(req.body) 
-        carModify._id = id 
-        const carUpdated = await Car.findByIdAndUpdate(id , carModify)
-        return res.status(200).json(carUpdated)
-    } catch (error) {
-        return next(error)
-    }
+router.get('/:id', async (req, res) => {
+	const id = req.params.id;
+	try {
+		const car = await Car.findById(id);
+		if (car) {
+			return res.status(200).json(car);
+		} else {
+			return res.status(404).json('Car not found with this id');
+		}
+	} catch (err) {
+		return res.status(500).json(err);
+	}
 });
   
+router.get('/modelo/:modelo', async (req, res) => {
+	const {modelo} = req.params;
 
-router.get("/marca/:marca", async (req, res) => {
-  const { marca } = req.params;
-
-  try {
-    const carMarca = await Car.find({ marca: marca });
-    return res.status(200).json(carMarca);
-  } catch (err) {
-    return res.status(500).json(err);
-  }
+	try {
+		const carModelo = await Car.find({ modelo: modelo });
+		return res.status(200).json(carModelo);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
 });
 
-router.post("/create", [filesMiddleware.upload.single("foto"), filesMiddleware.uploadToCloudinary],
-  async (req, res, next) => {
-    try {
-      const cloudinaryUrl = req.file_url ? req.file_url : null;
-      const { marca, modelo, potencia, mercado = "Europeo" } = req.body;
-      const car = {
-        marca,
-        modelo,
-        potencia,
-        mercado,
-        foto: cloudinaryUrl,
-      };
-      const newCar = new Car(car);
-      const createdCar = await newCar.save();
-      return res.status(201).json(createdCar);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+router.get('/marca/:marca', async (req, res) => {
+	const {marca} = req.params;
 
-router.delete("/delete/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    await Car.findByIdAndDelete(id);
-    return res.status(200).json("Car deleted");
-  } catch (error) {
-    return next(error);
-  }
+	try {
+		const carMarca = await Car.find({ marca: marca });
+		return res.status(200).json(carMarca);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
 });
+
+router.post('/create', [filesMiddleware.upload.single('foto'), filesMiddleware.uploadToCloudinary], async (req, res, next) => {
+	try {
+	  const cloudinaryUrl = req.file_url ? req.file_url : null;
+	  const { marca, modelo, potencia, mercado = 'Europeo' } = req.body;
+	  const car = {
+		marca,
+		modelo,
+		potencia,
+		mercado,
+		foto: cloudinaryUrl
+	  };
+	  const newCar = new Car(car);
+	  const createdCar = await newCar.save();
+	  return res.status(201).json(createdCar);
+	} catch (error) {
+	  next(error);
+	}
+  });
+  
+router.delete('/delete/:id', async (req, res, next) => {
+	try {
+	  	const { id } = req.params;
+	  	await Car.findByIdAndDelete(id);
+	  	return res.status(200).json('Car deleted');
+	} catch (error) {
+	  	return next(error);
+	}
+  });
+  
 
 module.exports = router;
